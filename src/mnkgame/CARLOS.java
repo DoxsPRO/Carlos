12c7080b6c1c36b712d23574a9c098d8533cd3b7
SIJ82 <si-j@outlook.it>
2021-09-10 23:50:33 +0200

    Codice commentato

--- src/mnkgame/CARLOS.java
+++ src/mnkgame/CARLOS.java
@@ -3,7 +3,6 @@ package mnkgame;
 import java.util.Random;
 
 public class CARLOS implements MNKPlayer{
-	private Random rand;
 	private MNKBoard B;
 	private MNKGameState myWin;
 	private MNKGameState yourWin;
@@ -11,7 +10,6 @@ public class CARLOS implements MNKPlayer{
 	long start;
 	
 	public CARLOS(){
-		
 	}
 	
 	public String playerName() {
@@ -20,7 +18,6 @@ public class CARLOS implements MNKPlayer{
 	
 	public void initPlayer(int M, int N, int K, boolean first, int timeout_in_secs) {
 		// New random seed for each game
-		rand    = new Random(System.currentTimeMillis()); 
 		B       = new MNKBoard(M,N,K);
 		myWin   = first ? MNKGameState.WINP1 : MNKGameState.WINP2; 
 		yourWin = first ? MNKGameState.WINP2 : MNKGameState.WINP1;
@@ -29,27 +26,27 @@ public class CARLOS implements MNKPlayer{
 	
 	public MNKCell selectCell(MNKCell[] FC, MNKCell[] MC) {
 		long start = System.currentTimeMillis();
+		
 		if(MC.length > 0) {
 			MNKCell c = MC[MC.length-1]; // Recover the last move from MC
 			B.markCell(c.i,c.j);         // Save the last move in the local MNKBoard
 		}
+		
 		// If there is just one possible move, return immediately
 		if(FC.length == 1)
 			return FC[0];
-		 
-		int pos   = rand.nextInt(FC.length); 
-		MNKCell c = FC[pos]; // random move
-		
-		c=BestMove(B, FC);
+
+		MNKCell c = BestMove(B, FC);
 		
 		B.markCell(c.i, c.j);
-		
-		// No win or loss, return the randomly selected move
+
 		return c;
 	}
 	
 	MNKBoard Copy(MNKBoard board)
 	{
+		//crea una copia della board
+		
 		/*MNKBoard cBoard=new MNKBoard(board.M, board.N, board.K);
 		
 		for(MNKCell c : board.getMarkedCells())
@@ -67,13 +64,14 @@ public class CARLOS implements MNKPlayer{
 		int bestScore=Integer.MIN_VALUE;
 		MNKCell bestMove=FC[0];
 		
+		
+		//Per ogni cella libera verifica la mossa migliore
 		for(MNKCell c : board.getFreeCells())
 		{
 			board.markCell(c.i, c.j);
 			int score=MiniMax(Copy(board), 3, false, Integer.MIN_VALUE, Integer.MAX_VALUE);
 			
 			//System.out.println("Carlos "+c.i+"-"+c.j+" "+score);
-			
 			
 			if(score>bestScore)
 			{
@@ -92,6 +90,7 @@ public class CARLOS implements MNKPlayer{
 	
 	public int MiniMax(MNKBoard board, int depth, boolean isMax, int alpha, int beta)
 	{
+		//se il tempo sta finendo ritorna 0
 		if((System.currentTimeMillis()-start)/1000.0 > TIMEOUT*(90.0/100.0))
 			return 0;
 		
@@ -126,7 +125,6 @@ public class CARLOS implements MNKPlayer{
 				if(beta <= alpha)
 					break;
 			}
-			
 			return maxScore;			
 		}
 		else
@@ -143,11 +141,7 @@ public class CARLOS implements MNKPlayer{
 			    if(beta <= alpha)
 			    	break;
 			}
-			
 			return minScore;
 		}
-		
 	}
-	
-	
 }
